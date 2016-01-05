49932542f11fc34a81e52df4e493ba2c45022826
Thomas Lumley <t.lumley@auckland.ac.nz>
2016-01-06 10:45:59 +1300

    hexbin and alpha-blended graphs

---
+++ figure1.R
@@ -0,0 +1,13 @@
+library(survey)
+load("combined-data.rda")
+
+dim(des)
+dim(subset(des, !is.na(BPXDAR) & !is.na(RIDAGEYR)))
+
+pdf("hexbp.pdf")
+svyplot(BPXDAR~RIDAGEYR,style="hex",design=des,legend=0,xlab="Age (yrs)",ylab="Diastolic BP (mmHg)")
+dev.off()
+
+pdf("alphabp.pdf")
+svyplot(BPXDAR~RIDAGEYR,style="trans",design=des,legend=0,xlab="Age (yrs)",ylab="Diastolic BP (mmHg)",pch=19)
+dev.off()
