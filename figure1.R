0d3e74df65a314b3338eb83dedb63e35ac6e3c83
Thomas Lumley <t.lumley@auckland.ac.nz>
2016-01-06 10:47:46 +1300

    fix page size

--- figure1.R
+++ figure1.R
@@ -8,6 +8,6 @@ pdf("hexbp.pdf")
 svyplot(BPXDAR~RIDAGEYR,style="hex",design=des,legend=0,xlab="Age (yrs)",ylab="Diastolic BP (mmHg)")
 dev.off()
 
-pdf("alphabp.pdf")
+pdf("alphabp.pdf",height=5,width=5)
 svyplot(BPXDAR~RIDAGEYR,style="trans",design=des,legend=0,xlab="Age (yrs)",ylab="Diastolic BP (mmHg)",pch=19)
 dev.off()
